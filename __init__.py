b5de3db4b959732d6a15e5ffa4c8b7945bd4c873
Cremuss <ghislainatcremuss@gmail.com>
2025-07-18 00:02:28 +0200

    Fixed preset

--- __init__.py
+++ __init__.py
@@ -12,12 +12,12 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 bl_info = {
-    "name": "Data Baker",
-    "author": "Ghislain GIRARDOT, Joshua BOGART, George VOGIATZIS (Gvgeo)",
-    "version": (1, 1),
-    "blender": (4, 2, 0),
-    "location": "View 3D -> Bake Data",
-    "description": "Enables you to bake all kind of data (pivots, axis, shapekey offset/normals, linear mask, spherical mask and more, into UVs or Vertex Color",
+    "name": "Template Addon",
+    "author": "Ghislain GIRARDOT",
+    "version": (1, 0),
+    "blender": (4, 3, 2),
+    "location": "",
+    "description": "Template addon",
     "warning": "",
     "doc_url": "",
     "tracker_url": "",
@@ -25,73 +25,20 @@ bl_info = {
 }
 
 import bpy
-from bpy.props import PointerProperty
-from bpy.types import PropertyGroup
 import os
-import shutil
 
 from . import auto_load
-
-def get_writable_preset_path(subdir: str) -> str:
-    """
-    Return the first preset folder we have permission to write to, given a preset subdir, excluding the vscode_development
-
-    :param subdir: preset sub directory to look for
-    :return: folder path
-    :rtype: str
-    """
-    for path in bpy.utils.preset_paths("operator"):
-        target_dir = os.path.join(path, subdir)
-
-        if 'vscode_development' in path:
-            continue
-
-        if not os.path.exists(target_dir):
-            try:
-                os.makedirs(target_dir, exist_ok=True)
-            except PermissionError:
-                continue
-
-        try:
-            test_file = os.path.join(target_dir, "temp_test.txt")
-            with open(test_file, 'w') as f:
-                f.write("test")
-            os.remove(test_file)
-            return target_dir
-        except (PermissionError, OSError):
-            continue
-
-    return None
-
-def install_default_presets():
-    """
-    Copy .py preset files bundled with the extension to the user preset folder, if not already in there
-    """
-
-    preset_subdirs = [
-        "ghistemplateaddon",
-    ]
-
-    for preset_subdir in preset_subdirs:
-        writable_path = get_writable_preset_path(preset_subdir)
-        if writable_path is None:
-            print("Warning: No writable preset path found.")
-            return
-
-        addon_preset_path = os.path.join(os.path.dirname(__file__), "presets", preset_subdir)
-        if not os.path.exists(addon_preset_path):
-            os.makedirs(addon_preset_path)
-
-        for file in os.listdir(addon_preset_path):
-            src = os.path.join(addon_preset_path, file)
-            dst = os.path.join(writable_path, file)
-            if not os.path.exists(dst):
-                shutil.copy2(src, dst)
 
 auto_load.init()
 
 def register():
     auto_load.register()
 
+    if register_preset_path := getattr(bpy.utils, "register_preset_path", None): # @TODO check
+        register_preset_path(os.path.join(os.path.dirname(__file__)))
+
 def unregister():
     auto_load.unregister()
+
+    if unregister_preset_path := getattr(bpy.utils, "unregister_preset_path", None):
+        unregister_preset_path(os.path.join(os.path.dirname(__file__)))
